a2dd3a47394ce5a145d67a229f28563c01fef2de
beaulucas <statsbeau@gmail.com>
2017-12-14 13:59:18 -0800

    First commit, some ggplot2 themes

---
+++ R/theme_glu.R
@@ -0,0 +1,16 @@
+##########
+#' glu theme
+#'
+#' @export
+#' @name theme_glu
+#' @author Beau Lucas
+#' @title Glu theme for ggplot2
+#' @examples
+
+
+theme_glu <- function () {
+  ggplot2::theme_minimal(base_size = 11)
+  }
+
+
+
